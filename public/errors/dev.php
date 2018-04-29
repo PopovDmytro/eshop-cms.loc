13a48ad46df704b6b11ccfdbd3da99ea021f7554
PopovDmytro <dimahom@ukr.net>
2018-04-29 20:47:24 +0300

    ErrorHandler

---
+++ public/errors/dev.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ERROR</title>
+</head>
+<body>
+
+<h1>ERROR</h1>
+<p><b>ERROR code: </b><?php echo $errno;?></p>
+<p><b>ERROR text: </b><?php echo $errstr;?></p>
+<p><b>ERROR file: </b><?php echo $errfile;?></p>
+<p><b>ERROR line: </b><?php echo $errline;?></p>
+
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -4,5 +4,6 @@ require_once dirname(__DIR__ ). "/config/init.php";
 require_once LIBS . '/functions.php';
 
 new \eshop\App();
-\eshop\App::$app->setProperty('test', 'test');
-debug(\eshop\App::$app->getProperties());
+
+throw new Exception('Message lorem lorem', 404);
+
