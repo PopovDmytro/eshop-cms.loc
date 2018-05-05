90a3d2c8be583625ccaa967fc75be3ec13edecbd
PopovDmytro <dimahom@ukr.net>
2018-05-05 10:28:11 +0300

    Breadcrumbs

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\Breadcrumbs;
 use app\models\Product;
 
 class ProductController extends AppController {
@@ -14,28 +15,30 @@ class ProductController extends AppController {
         }
 
         //breadcrumbs
+        $breadcrumbs = Breadcrumbs::getBreadcrumbs($product->category_id, $product->title);
 
         //bundle products
         $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ? ", [$product->id]);
+
         //write in cookies viewed product
         $p_model = new Product();
         $p_model->setRecentlyViewed($product->id);
+
         //viewed products
         $r_viewed = $p_model->getRecentlyViewed();
         $recentlyViewed = null;
         if($r_viewed) {
             $recentlyViewed = \R::find('product', 'id IN (' . \R::genSlots($r_viewed) . ') LIMIT 4 ', $r_viewed);
-        } else {
-
         }
 
         //gallery
         $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
+
         //product modifications
 
 
 
         $this->setMeta($product->title, $product->descriprion, $product->keywords);
-        $this->set(compact('product', 'related', 'gallery', 'recentlyViewed'));
+        $this->set(compact('product', 'related', 'gallery', 'recentlyViewed', 'breadcrumbs'));
     }
 }
